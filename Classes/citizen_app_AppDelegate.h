5f30fa7f6917c5d6ce70feb512c1b7920f446bd8
Pavan Kiran Rai <pavan.kiran.rai@sap.com>
2017-10-17 11:49:08 +0530

    Updating code

---
+++ Classes/citizen_app_AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  citizen_app_worldAppDelegate.h
+//  citizen_app_world
+//
+//  Created by Taylor Carpenter on 8/7/10.
+//  Copyright (c) 2010 CodeCafe. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface citizen_app_worldAppDelegate : NSObject <UIApplicationDelegate> {
+
+    UIWindow *window;
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iOS-Citizen-App
 iOS App for uploading pictures and status tracking
+
+You can open in xocde
